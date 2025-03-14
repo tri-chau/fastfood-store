f569d8526aa72cd02063a94c08362c7a2f69f73a
tri-chau <trichau2511@gmail.com>
2025-03-14 12:36:49 +0700

    replace public folder

--- public/build/assets/InputElement-BuGiFTyO.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./index-B6AZzZ4c.js";const p=({style:t,type:s,placeholder:e,onChange:n,name:r})=>m.jsx("input",{className:t,type:s,placeholder:e,onChange:n,name:r});export{p as I};
