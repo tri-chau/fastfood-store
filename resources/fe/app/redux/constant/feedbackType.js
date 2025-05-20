0777aed333533a9209669f1c61590e6c63da3720
AntomBD <58204379+Antombd@users.noreply.github.com>
2025-05-20 21:06:45 +0700

    add feedbackType.js

---
+++ resources/fe/app/redux/constant/feedbackType.js
@@ -0,0 +1,31 @@
+// get single feedback by id
+export const GET_FEEDBACK_PROCESS = "GET_FEEDBACK_PROCESS";
+export const GET_FEEDBACK_SUCCESS = "GET_FEEDBACK_SUCCESS";
+
+// get all feedbacks
+export const GET_FEEDBACKS_PROCESS = "GET_FEEDBACKS_PROCESS";
+export const GET_FEEDBACKS_SUCCESS = "GET_FEEDBACKS_SUCCESS";
+
+// Get feedbacks by rating
+export const GET_FEEDBACKS_BY_RATING_PROCESS =
+  "GET_FEEDBACKS_BY_RATING_PROCESS";
+export const GET_FEEDBACKS_BY_RATING_SUCCESS =
+  "GET_FEEDBACKS_BY_RATING_SUCCESS";
+
+// search a product by name and filter it by rating and price
+// export const SEARCH_RESULT_PROCESS = "SEARCH_RESULT_PROCESS ";
+// export const SEARCH_RESULT_SUCCESS = "SEARCH_RESULT_SUCCESS ";
+
+// return the value of product title as a search result on input
+// export const INPUT_RESULT_PROCESS = "INPUT_RESULT_PROCESS ";
+// export const INPUT_RESULT_SUCCESS = "INPUT_RESULT_SUCCESS ";
+
+
+// feedback CRUD
+export const CREATE_FEEDBACK_PROCESS = "CREATE_FEEDBACK_PROCESS";
+export const CREATE_FEEDBACK_SUCCESS = "CREATE_FEEDBACK_SUCCESS";
+
+export const UPDATE_FEEDBACK_PROCESS = "UPDATE_FEEDBACK_PROCESS";
+export const UPDATE_FEEDBACK_SUCCESS = "UPDATE_FEEDBACK_SUCCESS";
+
+export const DELETE_FEEDBACK_PROCESS = "DELETE_FEEDBACK_PROCESS";
