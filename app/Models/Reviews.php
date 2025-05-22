799c3ed13a2c9fe275be31dfaff9040990727e3a
AntomBD <58204379+Antombd@users.noreply.github.com>
2025-05-22 23:27:15 +0700

    add reviews model

---
+++ app/Models/Reviews.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+class Reviews extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id',
+        'customer_id',
+        'product_id',
+        'order_detail_id',
+        'rating',
+        'comment',
+        'is_edited',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'customer_id', 'id');
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+    public function orderDetail()
+    {
+        return $this->belongsTo(OrderDetail::class, 'order_detail_id', 'id');
+    }
+}
